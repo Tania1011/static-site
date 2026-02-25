58fca38a653bd927326e612d3c2220a553fe3787
Tania1011 <tania.pereminski@gmail.com>
2026-02-25 14:41:48 -0800

    Add changeMessage function to modify div properties
    Implement changeMessage function to update div text, background color, and add CSS class.

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+  function changeMessage () {
+      // Change the text inside the div
+      box.textContent = " You clicked! I have been changed by JavaScript!";
 
+      // Change the background color using inline CSS
+      box.style.backgroundColor = "lightgreen";
+
+      //Add a CSS class (defined in style.css)
+      box.classList.add("success-box");
+  }   
